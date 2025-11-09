922413a6f9146cb89f2f051d6f2f5a52d618a592
Alexandra Apró <aleksandra.apro97@gmail.com>
2025-11-09 13:23:03 +0100

    Add connection

---
+++ Workshops/diagnose.py
@@ -0,0 +1,8 @@
+# diagnose.py
+def diagnose(image_path: str) -> str:
+    """
+    image_path -> a lementett kép elérési útja
+    visszatérés: 'APPLE' / 'BANANA' / 'UNKNOWN' / bármi más string
+    """
+    # TODO: ide jöhet majd a saját logikád (képfelismerés, színvizsgálat stb.)
+    return "APPLE"
